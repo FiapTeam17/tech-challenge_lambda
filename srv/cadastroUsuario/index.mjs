7f6afd7b2e1b63133043564490abaea4d3aa9f7f
André Paes França <andrepaesfranca@gmail.com>
2023-10-31 22:40:40 -0300

    fix: conexao

--- srv/cadastroUsuario/index.mjs
+++ srv/cadastroUsuario/index.mjs
@@ -1,23 +1,24 @@
 import * as mysql from "mysql2/promise";
 
 export const handler = async (event, context) => {
+    // Crie uma conexão com o banco de dados RDS
+    const connection = await mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_SCHEMA,
+    });
+
     try {
         const jsonInsert = {
             nome: event["nome"],
             cpf: event["cpf"],
             email: event["email"],
         };
-        // Crie uma conexão com o banco de dados RDS
-        const connection = await mysql.createConnection({
-            host: process.env.DB_HOST,
-            user: process.env.DB_USERNAME,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_SCHEMA,
-        });
 
         // Execute uma consulta
         const [result] = await connection.execute(
-            "INSERT INTO Cliente (nome, cpf, email) VALUES (?, ?, ?, ?)",
+            "INSERT INTO Cliente (nome, cpf, email) VALUES (?, ?, ?)",
             [jsonInsert.nome, jsonInsert.cpf, jsonInsert.email]
         );
     } catch (error) {
